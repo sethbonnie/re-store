9afe8067cc8f8e0e17f68659cfb81758eeda31af
Seth Bonnie <seth@lifecorp.com>
2015-06-01 16:57:08 -0500

    Remove persistent-method-events
    It's unnecessary since I'm already testing the events inside the
    persistent methods file.

--- test/List.js
+++ test/List.js
@@ -1,6 +1,5 @@
 var likeImmutableList = require( './like-immutable-list' );
 var mutablePersistentListMethods = require( './mutable-persistent-list-methods' );
-var persistentMethodEvents = require( './persistent-method-events' );
 var historyMethods = require( './history-methods' );
 
 describe( 'Restore.ListStore()', function() {
@@ -8,8 +7,6 @@ describe( 'Restore.ListStore()', function() {
   describe( 'behaves like an Immutable.List', likeImmutableList );
 
   describe( 'Mutable Persistent List Methods', mutablePersistentListMethods );
-
-  //describe( 'Persist Method Events', persistentMethodEvents.bind(null, ListStore) );
 
   //describe( 'History', historyMethods.bind(null, ListStore) );
 });
--- test/persistent-method-events.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by skb on 5/24/2015.
- */
